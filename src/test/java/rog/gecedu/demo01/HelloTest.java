8f4b104d89846be7f684b9e9e551bfa88ac713ac
“zhangsan <276221755@qq.com>
2019-03-06 23:50:36 +0800

    maven web提交

---
+++ src/test/java/rog/gecedu/demo01/HelloTest.java
@@ -0,0 +1,14 @@
+package rog.gecedu.demo01;
+
+import org.junit.Test;
+
+public class HelloTest {
+	
+	@Test
+	public void testname() throws Exception {
+		/*
+		 * Hello hello = new Hello(); hello.show();
+		 */
+	}
+
+}
